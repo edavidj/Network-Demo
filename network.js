b721bd373cbac235a4c7637d5b2a94bc0147564e
Ethan Johnston <ethan.johnston9@hotmail.com>
2017-07-12 19:20:23 -0400

    added tooltip feedback and cleaned code a little

--- network.js
+++ network.js
@@ -1,47 +1,57 @@
 var currentIndex = 1; //track id's as number of groups gets larger(need unique id's)
 var nodes = new vis.DataSet(); //initialize empty sets to add too
 var edges = new vis.DataSet();
+var container = document.getElementById('mynetwork'); 
+var data = {
+    nodes: nodes,
+    edges: edges
+};
+var options = {
+    nodes: {
+        shape: 'dot'
+    }    
+};
+var network = new vis.Network(container, data, options);
+
 var selected = { //object for tracking the currently selected node
     params: null,
     state:false,
     id:null,
     splitID:null
 }
-class nodeControl{
-    constructor(){ 
-        this.green = 0;      
+function nodeControl(){
+    this.green = 0;
+}
+nodeControl.prototype.nextShade = function(){
+    this.green = this.green + 48;
+}
+nodeControl.prototype.currentShade = function(id){
+    var split = (id==='') ? '' : id.split(' ');
+    var factor = 48*(split.length);        
+    if(factor>255){ //rgb cap TODO: change so it cycles all colors
+        factor=255;            
     }
-    nextShade(){
-        this.green = this.green + 48;
+    this.green = 0 + 36*(split.length);
+    return this.green;
+}
+nodeControl.prototype.currentValue = function(id){
+    var split = id.split(' ');        
+    var factor = 1*(split.length);
+    if(factor>100){
+        factor = 100;
     }
-    currentShade(id){        
-        var split = id.split(' ');
-        if(id===''){split='';}
-        var factor = 48*(split.length);        
-        if(factor>255){
-            factor=255;            
-        }
-        this.green = 0 + 36*(split.length);
-        return this.green;
-    }
-    currentValue(id){
-        var split = id.split(' ');        
-        var factor = 1*(split.length);
-        if(factor>100){
-            factor = 100;
-        }
-        return 100 - factor;
-    }
-} 
+    return 100 - factor;
+}
 var nodeProperties = new nodeControl(); //can be used to track nodeProperties of one thread or just to provide functions to track green rgba number
 var green = nodeProperties.currentShade('').toString();
 nodes.add({id:'0',value:100,title:"core",color:{background:'rgba(255,'+green+',0,1)',border:'rgba(255,'+green+',0,1)'},mass:4})
+
 function addGroup(){
-    if(nodes.getIds().length === 0){
+    if(nodes.getIds().length === 0){ //first node (if they deleted the starting node)
         var green = nodeProperties.currentShade('').toString();
         nodes.add({id:'0',value:100,title:"core",color:{background:'rgba(255,'+green+',0,1)',border:'rgba(255,'+green+',0,1)'},mass:4})
     }
-    else if(selected.state){
+    else if(selected.state){ //regular group creation
         nodeProperties.nextColor;
         var numberOfNodes = document.getElementById('nodeCount').value;
         for(var i = 0; i< numberOfNodes;i++){
@@ -49,7 +59,8 @@ function addGroup(){
         }
     }
     else{
-        alert("Please select a node before expanding.");
+        let message = "Please select a node before expanding!";
+        tooltipFeedback('addgroup',message,2000);
     }    
 }
 function createNode(nodeProperties){
@@ -76,20 +87,22 @@ function clearAll(){ //empty the network
     edges.clear();
     network.setData(data);
 }
+function tooltipFeedback(id, message, duration = 1500){
+    let element = '#'+id;
+    $(element).tooltip({
+        html:true,
+        title: message,
+        trigger: "manual"
+    });
+    $(element).tooltip("show");
+    setTimeout(function(){
+        $(element).tooltip("destroy");
+    },duration);    
+}
 infoStore = [];
 
 //createGroup(); //change later so that this is called on interaction
-var container = document.getElementById('mynetwork'); 
-var data = {
-    nodes: nodes,
-    edges: edges
-};
-var options = {
-    nodes: {
-        shape: 'dot'
-    }    
-};
-var network = new vis.Network(container, data, options);
+
 network.on("click", function (params) {      
     if(typeof params.nodes[0] !=='undefined'){
         selected.params = params;
